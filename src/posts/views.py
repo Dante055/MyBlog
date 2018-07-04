a4bb0de8931a67f3a56e68d0d435a9a7b8c5e05c
Abhijeet Maurya <mauryaabhijeet37@gmail.om>
2018-07-04 19:31:42 +0530

    Get Item or 404 Query ; Dynamic Url Routing

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Post
+
 
 def post_list(request):
     queryset = Post.objects.all()
@@ -17,8 +18,16 @@ def post_create(request):
     return HttpResponse("<h1>Create</h1>")
 
 
-def post_detail(request):
-    return HttpResponse("<h1>Detail</h1>")
+def post_detail(request, id=None):
+    # instance = Post.objects.get(id=4)
+    instance = get_object_or_404(Post, id=id)
+    context = {
+        'title': 'Detail',
+        'post_title': instance.title,
+        'instance': instance
+    }
+    return render(request, "post_detail.html", context)
+    # return HttpResponse("<h1>Detail</h1>")
 
 
 def post_update(request):
