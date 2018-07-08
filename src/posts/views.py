843f3854907e4ee3e15a413df814cae0b47ea07a
Abhijeet Maurya <mauryaabhijeet37@gmail.om>
2018-07-08 13:48:02 +0530

    Model Manager and Handling Drafts

--- src/posts/views.py
+++ src/posts/views.py
@@ -4,13 +4,19 @@ from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 
 from .forms import PostForm
 from .models import Post
 
 
+
+
 def post_list(request):
-    queryset_list = Post.objects.all()  # .order_by("-timestamp")
+    today = timezone.now().date()
+    queryset_list = Post.objects.active() # filter(draft=False).filter(publish__lte=timezone.now())#.order_by("-timestamp")
+    if request.user.is_staff or request.user.is_superuser:
+        queryset_list = Post.objects.all()
     paginator = Paginator(queryset_list, 10) # Show 10 contacts per page
     page_request_var = "page"
     page = request.GET.get(page_request_var)
@@ -26,7 +32,8 @@ def post_list(request):
     context = {
         "object_list": queryset,
         "title": "List",
-        "page_request_var": page_request_var
+        "page_request_var": page_request_var,
+        'today':today
     }
 
     return render(request, "post_list.html", context)
@@ -56,6 +63,9 @@ def post_create(request):
 def post_detail(request, slug=None):
     # instance = Post.objects.get(id=4)
     instance = get_object_or_404(Post, slug=slug)
+    if instance.draft or instance.publish > timezone.now().date():
+        if not request.user.is_staff or not request.user.is_superuser:
+            raise Http404
     share_string = quote_plus(instance.content)
     context = {
         "title": instance.title,
