d9ca3ffb3b686c06c87a5a104dcbafd98b442e4c
Abhijeet Maurya <mauryaabhijeet37@gmail.om>
2018-07-13 11:16:05 +0530

    Input groups.

--- src/posts/forms.py
+++ src/posts/forms.py
@@ -14,7 +14,7 @@ class PostForm(forms.ModelForm):
             "title",
             "content",
             "image",
-            "draft",
+            'draft',
             "publish",
         ]
 
